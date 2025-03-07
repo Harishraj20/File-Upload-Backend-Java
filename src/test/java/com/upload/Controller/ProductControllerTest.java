43f47c88bee9ca9e1f0a5c058efdfff3eeefe892
Harishraj20 <sharish29102002@gmail.com>
2025-03-07 18:31:56 +0530

    Added Test Cases for Controller

--- src/test/java/com/upload/Controller/ProductControllerTest.java
+++ src/test/java/com/upload/Controller/ProductControllerTest.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.hibernate.HibernateException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,8 +31,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.upload.Models.Product;
 import com.upload.Service.ProductService;
+import org.mockito.junit.MockitoJUnitRunner;
 
-class ProductControllerTest {
+@RunWith(MockitoJUnitRunner.class)
+public class ProductControllerTest {
 
     private MockMvc mockMvc;
 
@@ -44,27 +46,27 @@ class ProductControllerTest {
 
     private MockMultipartFile file;
 
-    @BeforeEach
-    void setUp() {
+    @SuppressWarnings("deprecation")
+    @Before
+    public void setUp() {
         file = new MockMultipartFile(
                 "image", "test.jpg", "image/jpeg", "dummy image content".getBytes());
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
     }
 
     @Test
-    void testHomePage() throws Exception {
+    public void testHomePage() throws Exception {
         mockMvc.perform(get("/home"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello Home Page!"));
     }
 
     @Test
-    void testHandleFileUpload_Success() throws Exception {
+    public void testHandleFileUpload_Success() throws Exception {
         Product product = new Product();
         when(productService.addProducts(any(Product.class))).thenReturn("Success");
         product.setStockStatus("true");
-        System.out.println("received file:   " + file);
 
         mockMvc.perform(multipart("/upload")
                 .file(file)
@@ -75,49 +77,23 @@ class ProductControllerTest {
 
         assertEquals("true", product.getStockStatus());
 
-        verify(productService, times(1)).addProducts((Product) any(Product.class));
-    }
-
-    @Test
-    void testHandleFileUploasdd_Success() throws Exception {
-        Product product = new Product();
-        product.setProductName("Test Product");
-        product.setStockStatus("true");
-
-        when(productService.addProducts(any(Product.class))).thenReturn("Success");
-
-        mockMvc.perform(multipart("/upload")
-                .file(file)
-                .param("productName", "Test Product")
-                .param("quantity", "10")
-                .param("capacity", "500ml"))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.message", is("Product added successfully")));
-
-        assertEquals("true", product.getStockStatus());
-
         verify(productService, times(1)).addProducts(any(Product.class));
     }
 
     @Test
-    void testHandleFileUploadWithEmptyFile_Failure() throws Exception {
+    public void testHandleFileUploadWithEmptyFile_Failure() throws Exception {
         MockMultipartFile emptyFile = new MockMultipartFile(
                 "image", "test.jpg", "image/jpeg", new byte[0]);
-
-        Product product = new Product();
-        product.setStockStatus("false");
-        product.setProductName("Test Product");
-        when(productService.addProducts(any(Product.class))).thenReturn("Success");
-        assertEquals("false", product.getStockStatus());
-        mockMvc.perform(multipart("/upload").file(emptyFile)
+    
+        mockMvc.perform(multipart("/upload")
+                .file(emptyFile)
                 .param("productName", "Test Product"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("File is empty")));
-
     }
-
+    
     @Test
-    void testFetchProducts() throws Exception {
+    public void testFetchProducts() throws Exception {
         Product product1 = new Product();
         product1.setProductName("Product 1");
 
@@ -137,27 +113,27 @@ class ProductControllerTest {
     }
 
     @Test
-    void handleFileUpload_HibernateException() throws Exception {
+    public void handleFileUpload_HibernateException() throws Exception {
         when(productService.addProducts(any(Product.class)))
                 .thenThrow(new HibernateException("Database error"));
+
         mockMvc.perform(multipart("/upload")
                 .file(file)
                 .param("productName", "Test Product"))
                 .andExpect(status().isInternalServerError())
                 .andExpect(jsonPath("$.message", is("Internal Server Error")));
-
     }
 
     @Test
-    void handleFileUpload_RunTimeException() throws Exception {
+    public void handleFileUpload_RunTimeException() throws Exception {
         when(productService.addProducts(any(Product.class)))
                 .thenThrow(new RuntimeException("Database error"));
+
         mockMvc.perform(multipart("/upload")
                 .file(file)
                 .param("productName", "Test Product"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("Something went wrong..Try Again Later!")));
-
     }
 
     @Test
@@ -178,8 +154,7 @@ class ProductControllerTest {
         String filename = "errorFile.jpg";
         when(productService.getImage(filename)).thenThrow(new RuntimeException("Simulated exception"));
 
-        mockMvc.perform(get("/" + filename)).andExpect(status().isInternalServerError());
-
+        mockMvc.perform(get("/" + filename))
+                .andExpect(status().isInternalServerError());
     }
-
 }
